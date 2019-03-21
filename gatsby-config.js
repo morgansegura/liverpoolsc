e73b98ca2ec1a3c14b0947968de86e3042c53fc4
Morgan Segura <morgansegura@gmail.com>
2019-03-21 15:23:43 -0700

    Fixes corrupt .env files

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,8 +131,8 @@ module.exports = {
     {
       resolve: `gatsby-source-prismic`,
       options: {
-        repositoryName: `liverpoolsc`,
-        accessToken: `MC5YSlB4WGhJQUFLOU41UFRP.Ou-_ve-_vSxSP1Lvv73vv71cZO-_vRpb77-977-9HxTvv73vv73vv73vv70QZBQV77-977-977-977-9SGI`,
+        repositoryName: `${process.env.PRISMIC_REPO}`,
+        accessToken: `${process.env.PRISMIC_API_KEY}`,
         linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
       },
     },
